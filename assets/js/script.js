7c5228075fdb443d4c8bda4c3dea9e35639c7d9f
abifauzannn <mohammadabifauzan64@gmail.com>
2023-11-06 17:58:59 +0700

    initial commit

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navList = document.querySelector('.nav-list');
-
-    // Event listener untuk tombol menu toggle
-    menuToggle.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
-});
